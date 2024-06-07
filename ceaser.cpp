e78cd6d4e2d40f702a3f7e41c3c4420adb1d0fe7
snehalghogare56 <118335168+snehalghogare56@users.noreply.github.com>
2024-06-07 09:06:33 +0530

    Create ceaser.cpp

---
+++ ceaser.cpp
@@ -0,0 +1,40 @@
+                           Secser cipher code
+Code:
+ #include <iostream>
+using namespace std;
+string encrypt(string text, int s)
+{
+	string result = "";
+
+	// traverse text
+	for (int i = 0; i < text.length(); i++) {
+		// apply transformation to each character
+		// Encrypt Uppercase letters
+		if (isupper(text[i]))
+			result += char(int(text[i] + s - 65) % 26 + 65);
+
+		// Encrypt Lowercase letters
+		else
+			result += char(int(text[i] + s - 97) % 26 + 97);
+	}
+
+	// Return the resulting string
+	return result;
+}
+
+// Driver program to test the above function
+int main()
+{
+	string text = "ATTACKATONCE";
+	int s = 4;
+	cout << "Text : " << text;
+	cout << "\nShift: " << s;
+	cout << "\nCipher: " << encrypt(text, s);
+	return 0;
+}
+
+
+Output:
+Text : ATTACKATONCE
+Shift: 4
+Cipher: EXXEGOEXSRGI
